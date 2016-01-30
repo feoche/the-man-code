294f405a9351d9d823c4d138d997a7970867f938
YoruNoHikage <yorunohikage@gmail.com>
2016-01-30 01:23:32 +0100

    Add Firebase dependency

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import { render } from 'react-dom';
+import Firebase from 'firebase';
+
+const myRootRef = new Firebase('https://code-de-conduite.firebaseIO.com/');
+myRootRef.set("hello world!");
 
 render(
   <p>Minimal project</p>,
