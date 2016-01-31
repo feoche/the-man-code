5aeb9e15ffd89c59173fd106535233694a099d5f
Francois EOCHE <francois.eoche@niji.fr>
2016-01-31 04:47:35 +0100

    Youtube integration

--- src/containers/Game.js
+++ src/containers/Game.js
@@ -1,15 +1,62 @@
 import React, { Component } from 'react';
+import YouTube from 'react-youtube';
 
 export default class Game extends Component {
+  constructor() {
+    super();
+    this.state = {
+      nextQuestion: false,
+      showSolution: false,
+      situationUrl: 'U6HvY9GPApk',
+      answerUrl: '2g811Eo7K8U'
+    }
+  }
+
   componentWillMount() {
     console.log('fetch data here');
   }
 
+  showSolutionVideo() {
+    if (this.state.showSolution) {
+      this.setState({nextQuestion: true});
+    } else {
+      this.setState({showSolution: true});
+    }
+  }
+
   render() {
+    const opts = {
+      playerVars: { // https://developers.google.com/youtube/player_parameters
+        autohide: 1,
+        autoplay: 1,
+        controls: 0,
+        disablekb: 1,
+        enablejsapi: 1,
+        fs: 0,
+        iv_load_policy: 3,
+        modestbranding: 1,
+        rel: 0,
+        showinfo: 0,
+        theme: 'dark'
+      }
+    };
+    const buttons = (
+      <div>
+        <span><a className="item error">Un beau gosse</a></span>
+        <span><a className="item active">Un quetard qui pique des Ferraris</a></span>
+        <span><a className="item success">Une moustache avant tout</a></span>
+        <span><a className="item">Obi-Wan Kenobi</a></span>
+      </div>
+    );
     return (
       <div className="game-main">
         <div className="visual">
-          <img src="https://static-secure.guim.co.uk/sys-images/Guardian/About/General/2013/11/1/1383329924748/Tom-Selleck-as-Magnum-PI-010.jpg" />
+          <YouTube
+            videoId={this.state.showSolution? this.state.answerUrl : this.state.situationUrl}
+            className="video"
+            opts={opts}
+            onEnd={this.showSolutionVideo.bind(this)}
+            />
         </div>
         <ul className="progress-bar">
           <li></li>
@@ -28,10 +75,7 @@ export default class Game extends Component {
             Je suis Tom Selleck, que suis-je vraiment?
           </div>
           <div className="answers">
-            <span><a className="item error">Un beau gosse</a></span>
-            <span><a className="item active">Un quetard qui pique des Ferraris</a></span>
-            <span><a className="item success">Une moustache avant tout</a></span>
-            <span><a className="item">Obi-Wan Kenobi</a></span>
+            {this.state.nextQuestion ? <span><a className="item">Next&nbsp;&rarr;</a></span> : buttons}
           </div>
         </div>
       </div>
